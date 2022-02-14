918c5b1b03cff92c1dd03d1fb89a7c8550076be9
Samsomyajit <somyajitchppr@gmail.com>
2022-02-14 06:49:59 +0000

    new updaes on user-management

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './components/signup/Signup';
+import Login from './components/login/Login';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+      <Navbar />
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/signup' element={<Signup/>} />
+            <Route path='/login' element={<Login/>} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ reactapp/src/components/navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+} from './NavbarElements';
+
+  
+const Navbar = () => {
+    const divStyle = {
+        width: '8%',
+        height:'50px',
+        float: 'left',
+      };
+      
+  return (
+    <>
+      <Nav>
+      <img className='nav-logo' src='/home/coder/project/workspace/reactapp/src/components/navbar/admissionportallogo.jpg' alt="" style={divStyle} />
+        <Bars />
+        <NavMenu>
+          <NavLink to='/' activeStyle>
+            Home
+          </NavLink>
+          <NavLink to='/signup' activeStyle>
+            Sign Up
+          </NavLink>
+          <NavLink to='/login' activeStyle>
+            Log In
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
