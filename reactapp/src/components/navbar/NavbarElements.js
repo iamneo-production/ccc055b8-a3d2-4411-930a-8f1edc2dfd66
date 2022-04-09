9314dbde8b3d1e2140bc138aa5545bc337e1068f
P. Sai Abhinash <69082776+SaiAbhinash@users.noreply.github.com>
2022-04-09 13:05:30 +0530

    Update NavbarElements.js

--- reactapp/src/components/navbar/NavbarElements.js
+++ reactapp/src/components/navbar/NavbarElements.js
@@ -14,7 +14,7 @@ export const Nav = styled.nav`
   /* justify-content: flex-start; */
 `;
   
-export const NavLink = styled(Link)`
+export const NavLink1 = styled(Link)`
   color: white;
   display: flex;
   align-items: center;
@@ -27,7 +27,7 @@ export const NavLink = styled(Link)`
   }
 `;
   
-export const Bars = styled(FaBars)`
+export const Bars1 = styled(FaBars)`
   display: none;
   color: #808080;
   @media screen and (max-width: 768px) {
@@ -41,7 +41,7 @@ export const Bars = styled(FaBars)`
   }
 `;
   
-export const NavMenu = styled.div`
+export const NavMenu1 = styled.div`
   display: flex;
   align-items: center;
   margin-right: -24px;
